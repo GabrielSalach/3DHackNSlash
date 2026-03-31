5ed6cff02e8a148fc81fa1fb8a72e12697955904
GabrielSalach <gabsalach@orange.fr>
2026-04-01 00:04:27 +0200

    animation testing

---
+++ Player/Animations/AnimationController.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class AnimationController : AnimationTree
+{
+	
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
