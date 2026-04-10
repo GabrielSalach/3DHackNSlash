b615426c98e107a4ebbdb43387ce0c6d162e6d53
gabriel <gabsalach@orange.fr>
2026-04-10 22:51:17 +0200

    Added Noizz player model

--- Player/ModelRoot.cs
+++ Player/ModelRoot.cs
@@ -3,19 +3,19 @@ using Godot.Collections;
 
 public partial class ModelRoot : Node3D
 {
-    private System.Collections.Generic.Dictionary<string, BoneAttachment3D> boneAttachments = new System.Collections.Generic.Dictionary<string, BoneAttachment3D>();
-    [Export] private Array<BoneAttachment3D> bones;
+	private System.Collections.Generic.Dictionary<string, BoneAttachment3D> boneAttachments = new System.Collections.Generic.Dictionary<string, BoneAttachment3D>();
+	[Export] private Array<BoneAttachment3D> bones;
 
-    public override void _Ready()
-    {
-        foreach (BoneAttachment3D bone in bones)
-        {
-            boneAttachments.Add(bone.Name, bone);
-        }
-    }
+	public override void _Ready()
+	{
+		foreach (BoneAttachment3D bone in bones)
+		{
+			boneAttachments.Add(bone.Name, bone);
+		}
+	}
 
-    public BoneAttachment3D GetBoneAttachment(string name)
-    {
-        return boneAttachments[name];
-    }
+	public BoneAttachment3D GetBoneAttachment(string name)
+	{
+		return boneAttachments[name];
+	}
 }
